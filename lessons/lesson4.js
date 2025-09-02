be73f9c944b6bdac6172008a9dc207ef19a12049
Yana Kaznacheieva <yanakaznacheyeva@gmail.com>
2025-09-02 22:08:57 +0300

    Added lesson4

---
+++ lessons/lesson4.js
@@ -0,0 +1,25 @@
+//Relational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than equal
+// <= - less than equal
+
+var result = 10 > 5
+console.log(result)
+var result = 5 < 7
+console.log(result)
+var result = 5 >= 5
+console.log(result)
+var result = 6 >= 5
+console.log(result)
+var result = 3 <= 8
+console.log(result)
+var result = 3 <= 3
+console.log(result)
+
+//Equality operators
+
+var x = 1
+console.log(x == '1') // lose comparison
+console.log(x === '1') // strict comparison
