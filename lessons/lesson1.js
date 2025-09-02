9295972748379d8577a9b2cb16acac41faea8092
Yana Kaznacheieva <yanakaznacheyeva@gmail.com>
2025-09-02 19:37:02 +0300

    Added vars, constants, data

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,2 +1,25 @@
 // 1.Hello World
 console.log("Hello World!")
+
+// Variables
+var firstName = "Marta"
+let lastName = "Grant"
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex = "Male"
+console.log(age)
+age = "6"
+console.log(age)
+
+//constants
+const occupation = "Engineer"
+console.log(occupation)
+
+//data
+var middleName = "Olivia" //string
+var ageOfBorther = 25 //number
+var isSheMarried = false //boolean
+var yearsInMarriage = null //null
+var numberOfCars = undefined //undefined
